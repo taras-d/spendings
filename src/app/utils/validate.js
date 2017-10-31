1ec55d6bceb2ba745539ea8f5162b9df342fee5a
taras-d <taras.datsenko@gmail.com>
2017-10-31 17:37:29 +0200

    Password validator

--- src/app/utils/validate.js
+++ src/app/utils/validate.js
@@ -2,14 +2,28 @@ const validate = require('validate.js');
 
 const validators = validate.validators;
 
+// Presence validator default options
 validators.presence.options = { allowEmpty: false, message: 'is required' };
+
+// Email validator default options
+validators.email.options = { message: '^Invalid email address' };
+
+// Required validator - alias for presence validator
 validators.required = validate.validators.presence;
 
-validators.email.options = { message: '^Invalid email address' };
+// Password validator
+validators.password = function(value, options) {
+    options = validate.extend({}, this.options, options);
+    if (value && !options.pattern.test(value)) {
+        return 'should have 6-30 non-space characters';
+    }
+}
+validators.password.options = { pattern: /[^\s]{6,30}/ };
 
 export default function() {
     const errors = validate(...arguments);
 
+    // Return first error message for every property
     if (errors) {
         for (let field in errors) {
             errors[field] = errors[field][0];
