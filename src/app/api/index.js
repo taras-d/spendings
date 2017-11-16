39e0e6f136b7692cdcd5ad9400d750b7d81c0c2a
taras-d <taras.datsenko@gmail.com>
2017-11-16 22:21:09 +0200

    Spendings service

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -3,6 +3,7 @@ import Bottle from 'bottlejs';
 import TokenService from './tokenService';
 import ApiService from './apiService';
 import UserService from './userService';
+import SpendingService from './spendingService';
 import LoggerService from './loggerService';
 
 const bottle = new Bottle();
@@ -10,6 +11,7 @@ const bottle = new Bottle();
 bottle.service('tokenService', TokenService)
 bottle.service('apiService', ApiService, 'tokenService', 'loggerService');
 bottle.service('userService', UserService, 'apiService', 'tokenService');
+bottle.service('spendingService', SpendingService, 'apiService');
 bottle.service('loggerService', LoggerService);
 
 export default bottle.container;
